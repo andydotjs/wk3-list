a31e1950fb7e8f1ef5b9c252ba9b1ecfc280fac5
Andrew Smail <smailaj@gmail.com>
2026-04-06 21:42:08 +0100

    Version 0.1

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def static_files(path):
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
